e796079d43064ae66e4c50e8f37412e803a26ac0
altinawiwaseem <wasemm@live.com>
2023-06-26 02:29:30 +0200

    styling the header

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,24 @@ async function fetchProducts() {
 }
 
 fetchProducts();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const trigger = document.querySelector(".trigger");
+  const menuList = document.querySelector(".menu-list");
+  const closeBtn = document.querySelector(".close-btn");
+
+  trigger.addEventListener("click", function (e) {
+    e.preventDefault();
+    menuList.classList.toggle("active");
+  });
+
+  closeBtn.addEventListener("click", function () {
+    menuList.classList.remove("active");
+  });
+
+  document.addEventListener("click", function (e) {
+    if (!menuList.contains(e.target) && !trigger.contains(e.target)) {
+      menuList.classList.remove("active");
+    }
+  });
+});
